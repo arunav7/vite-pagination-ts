d653faa2f514f55d4dfb5e9a29236cd81d4a2070
arunav7 <harsharunav9@gmail.com>
2023-04-16 03:47:40 +0530

    added skeleton loader for posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Skeleton } from '@mui/material';
 
 import Pages from './components/Pages';
 import { Posts } from './types';
@@ -7,15 +8,29 @@ import './App.css';
 
 const App: React.FC = (): JSX.Element => {
   const [data, setData] = useState<Posts[]>([]);
-  // getting posts
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(data => setData(data as Posts[]));
+      .then(data => {
+        setTimeout(() => {
+          setData(data as Posts[]);
+        }, 3000);
+      });
   }, []);
+
   return (
     <div className="App">
-      {data && data.length > 0 ? <Pages data={data} /> : <p>Loading...</p>}
+      {data && data.length > 0 ? (
+        <Pages data={data} />
+      ) : (
+        <Skeleton
+          animation="wave"
+          variant="rectangular"
+          width={900}
+          height={600}
+        />
+      )}
     </div>
   );
 };
